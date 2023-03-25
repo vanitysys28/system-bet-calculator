a9892d7ed9bffdc8f03c62bcdaf19cfc5faf65f7
vanitysys28 <aubert.guillaume26@gmail.com>
2023-03-25 17:10:51 +0000

    adding binomial coeff to calculate unit stake and amount of generated bets

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,44 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+const binomialCoefficient = (k, n) => {
+  if (Number.isNaN(n) || Number.isNaN(k)) return NaN;
+  if (k < 0 || k > n) return 0;
+  if (k === 0 || k === n) return 1;
+  if (k === 1 || k === n - 1) return n;
+  if (n - k < k) k = n - k;
+  let res = n;
+  for (let j = 2; j <= k; j++) res *= (n - j + 1) / j;
+  return Math.round(res);
+};
+
+const systems = [2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+
+let success = '' 
+let trial = '' 
+let stake = ''
+
+$: rows = binomialCoefficient(success, trial);
+$: unitStake = (stake / rows).toFixed(2)
+</script>
+
+
+<h1>Calculateur Pari Système</h1>
+
+<select bind:value={success}>
+{#each systems as value}<option {value}>{value}</option>{/each}
+</select>
+
+<select bind:value={trial}>
+{#each systems as value}<option {value}>{value}</option>{/each}
+</select>
+
+<div>
+Mise totale:  <input bind:value={stake}>
+</div>
+
+<div>
+Nombre de paris: {#if rows > 1} {rows} {/if}
+</div>
+
+<div>
+Mise unitaire: {unitStake}
+</div>
