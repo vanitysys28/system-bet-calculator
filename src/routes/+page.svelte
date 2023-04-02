19757905a393784c2d0b3c921e12646fe52670d9
vanitysys28 <aubert.guillaume26@gmail.com>
2023-04-02 14:49:11 +0000

    calculating without generator function, outcomes consideration missing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,7 @@
 <script>
-const binomialCoefficient = (n, k) => {
+import * as C from 'js-combinatorics';
+
+const getBinomialCoefficient = (n, k) => {
   if (Number.isNaN(n) || Number.isNaN(k)) return null;
   if (k < 0 || k > n) return 0;
   if (k === 0 || k === n) return 1;
@@ -10,63 +12,48 @@ const binomialCoefficient = (n, k) => {
   return Math.round(res);
 };
 
-function* combinationN(array, n, outcomes) {
-  for (let i = 0; i <= array.length - n; i++) {
-    for (const c of combinationN(array.slice(i + 1), n - 1)) {
-      if(outcomes[i].outcome == "Gagné") {
-	yield [array[i] * c];
-	console.log(outcomes[i].outcome)
-      }
-      if(outcomes[i].outcome == "Void") {
-	yield [array[i] * 1];      
-}
-      if(outcomes[i].outcome == "Perdu") {
-	yield [array[i] * 0];      
-}
-    }
-  }
-}
-
-const arrayRange = (start, stop, step) =>
+const createArrayRange = (start, stop, step) =>
     Array.from(
     { length: (stop - start) / step + 1 },
     (value, index) => start + index * step
     );
 
-const systems = arrayRange(2,15,1);
+const systems = createArrayRange(2,15,1);
 const outcomes = ["Gagné","Perdu","Void"]
 
 let combinations = 2  
-let selections = 0 
+let selections 
 let combinationReturns = []
-let totalStake = 50 
+let totalStake   
 let totalReturn = 0 
 
-$: totalBets = binomialCoefficient(selections, combinations);
+$: totalBets = getBinomialCoefficient(selections, combinations);
 $: unitStake = (totalStake / totalBets).toFixed(2)
 let odds = []
 let values = []
 
 function addOdds(selections) {
 odds = new Array(selections).fill()
-odds.forEach((val, i) => {
+odds.forEach((odd, i) => {
      odds[i] = {"value": "1.8","outcome":"Gagné"};
  })
 }
 
-$: if (odds.every(element => element !== null)) {
-combinationReturns = [] 
-for (const c of combinationN(values, combinations, odds)) {
-combinationReturns.push(unitStake * c);
+$: combinationIterator = C.Combination.of(values, combinations);
+
+$: if (totalStake) {
+combinationReturns = []
+for (let i = 0; i <= [...combinationIterator].length - 1; i++) {
+combinationReturns.push([...combinationIterator][i].reduce( (a, b) => a * b ) * unitStake)
 }
 }
 
-$: odds, values = odds.map(a => a.value);
+$: odds, values = odds.map(odd => odd.value);
 $: odds, totalReturn = combinationReturns.reduce(function(a,b) { return a + b; }, 0).toFixed(2)
 </script>
 
 
-<h1>Calculateur Pari Système</h1>
+<h1 class="text-3xl font-bold underline">Calculateur Pari Système</h1>
 
 <select bind:value={combinations}>
 {#each systems as value}<option {value}>{value}</option>{/each}
@@ -95,13 +82,14 @@ Pari {i + 1}:  <input bind:value={bet.value} placeholder="">
 </div>
 
 <div>
-Nombre de paris générés: {#if totalBets > 1} {totalBets} {/if}
+Nombre de paris générés: {#if totalBets > 0} {totalBets} {/if}
 </div>
 
 <div>
-Mise unitaire: {unitStake}
+Mise unitaire: {#if unitStake > 0} {unitStake} {/if}
 </div>
 
 <div>
 Gains: {totalReturn}
 </div>
+
